a4ee7052acc61a8a5435291618be716a7fff2869
Patrick Wang <patrickkwang@users.noreply.github.com>
2023-01-08 20:10:37 -0500

    Improve test coverage

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,12 @@
 """Example tests."""
-from src.example import main
+from src.example import main, do_something
 
 
 def test_example() -> None:
     """Test example."""
     main()
+
+
+def test_something():
+    """Test something else."""
+    do_something()
