11c1f46163e8ef9be870967be418473d7e7ac10c
Patrick Wang <1263870+patrickkwang@users.noreply.github.com>
2023-01-09 20:14:12 -0500

    Add test coverage job
    Co-authored-by: Patrick Wang <patrickkwang@users.noreply.github.com>

--- src/example.py
+++ src/example.py
@@ -1,9 +1,19 @@
-"""Example script."""
+"""Test example.
+
+Other things.
+"""
 
 
 def main() -> None:
     """Run the example."""
     pass
+
+
+def do_something() -> None:
+    """Do something else."""
+    a = 5
+    b = a * 3
+    c = a + b
 
 
 if __name__ == "__main__":
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,12 @@
 """Example tests."""
-from src.example import main
+from example import main, do_something
 
 
 def test_example() -> None:
     """Test example."""
     main()
+
+
+def test_something() -> None:
+    """Test something else."""
+    do_something()
